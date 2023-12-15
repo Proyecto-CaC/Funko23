c02b4bffe7a19ec7b10ec2a5b1cab4ccc45c3a5d
Gui1e <gep2@protonmail.com>
2023-12-15 03:20:58 -0300

    localhost and db working

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,13 +1,12 @@
-const getAllProducts = async () => {
-    try {
-        const [productos] = await pool.query("SELECT product.product_id, product.product_name, product.product_description,product.price,product.stock,product.sku,product.dues,product.image_front,product.image_back,product.create_time,licence.licence_name,product.category_id FROM product JOIN licence ON product.licence_id = licence.licence_id;")
-        
-        return productos;
-    } catch (error) {
-        console.error('Error querying MySql:', error);
-        throw error;
-    } 
+const {conn} = require('../config/conn')
+
+
+const getAll = async () => {
+   
+        const [rows] = await conn.query('SELECT * FROM product;');
+        return rows;
 }
+
 module.exports = {
-    getAllProducts,
+    getAll
 }
